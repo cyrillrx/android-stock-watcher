29f4e4b6d265a6707205f386aa90e870b6c84317
Cyril Leroux <cyril0leroux@gmail.com>
2022-03-04 16:41:52 +0100

    Rename AlerterTheme into AppTheme

--- app/src/main/java/com/cyrillrx/alerter/MainActivity.kt
+++ app/src/main/java/com/cyrillrx/alerter/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.cyrillrx.alerter.ui.theme.AlerterTheme
+import com.cyrillrx.alerter.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AlerterTheme {
+            AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
                     Greeting("Android")
@@ -34,7 +34,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    AlerterTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
